e46db21c957e442c3c4ec16ef39bc673bc7bf93e
Mankov Konstantin <k.mankov@cft.ru>
2020-08-25 14:47:04 +0700

    slider

--- js/main.js
+++ js/main.js
@@ -1,1 +1,11 @@
 new WOW().init();
+
+const mySwiper = new Swiper('.swiper-container', {
+    scrollbar: {
+      el: '.swiper-scrollbar',
+      draggable: true,
+    },
+  });
+
+// Now you can use all slider methods like
+// mySwiper.slideNext();
